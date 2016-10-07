529e793dc1f479ce2ffbbcf618f21f57e302a47f
Jasper Ginn <jasperginn@hotmail.com>
2016-10-07 18:58:43 +0200

    Almost ready with scheduler

--- scheduler.py
+++ scheduler.py
@@ -19,6 +19,7 @@ TODO:
 
 Add config file:
     * Add option to disable logging
+    * log location
     * Add interval to download tables (days)
     * Add interval to download clickstreams (days)
     * Yes/no for downloading clickstreams/tables
@@ -52,7 +53,10 @@ class call:
     
     def __init__(self, course_slug):
         self.course_slug = course_slug
-    
+        self.interval = [datetime.date.today() - datetime.timedelta(days=7 # Change to number of days in config), # If you're running it on friday, you want the results from
+                         datetime.date.today() - datetime.timedelta(days=1)]: # Previous friday to yesterday.]
+        self.folder = os.chdir() + "/data/" + course_slug + "/"
+        
     '''
     Retrieve course id based on course slug
     '''
@@ -71,13 +75,13 @@ class call:
         # Get courseID
         course_id = json_data["elements"][0]["id"]
         # Return
-        return course_id
+        self.course_id = course_id
         
     '''
     Request sql data
     '''
     
-    def request_schemas(self, course_id, export_type = "RESEARCH_WITH_SCHEMAS", anonymity_level = "HASHED_IDS_NO_PII", 
+    def request_schemas(self, export_type = "RESEARCH_WITH_SCHEMAS", anonymity_level = "HASHED_IDS_NO_PII", 
                         statement_of_purpose = "Weekly backup of course data",
                         schema_names = ["demographics",	
                                         "users",	
@@ -94,7 +98,7 @@ class call:
         # Add log entry --> TODO
         
         # Construct request
-        er = ExportRequest(course_id=course_id, export_type=export_type, anonymity_level = anonymity_level, 
+        er = ExportRequest(course_id=self.course_id, export_type=export_type, anonymity_level = anonymity_level, 
                            statement_of_purpose = statement_of_purpose, schema_names = schema_names)
         # Fire request
         ERM = api.post(er)[0]
@@ -109,16 +113,14 @@ class call:
     Request clickstream data
     '''
     
-    def request_clickstream(self, course_id, export_type = "RESEARCH_EVENTING", anonymity_level = "HASHED_IDS_NO_PII", 
-                            statement_of_purpose = "Weekly backup of course data", 
-                            from_ = datetime.date.today() - datetime.timedelta(days=7), # If you're running it on friday, you want the results from
-                            to_ = datetime.date.today() - datetime.timedelta(days=1): # Previous friday to yesterday.
+    def request_clickstream(self, export_type = "RESEARCH_EVENTING", anonymity_level = "HASHED_IDS_NO_PII", 
+                            statement_of_purpose = "Weekly backup of course data")
                                             
         # Add log entry --> TODO
         
         # Construct request
-        er = ExportRequest(course_id=course_id, export_type=export_type, anonymity_level = anonymity_level, 
-                           statement_of_purpose = statement_of_purpose, interval=[str(from_), str(to_))
+        er = ExportRequest(course_id=self.course_id, export_type=export_type, anonymity_level = anonymity_level, 
+                           statement_of_purpose = statement_of_purpose, interval=self.interval)
         # Fire request
         ERM = api.post(er)[0]
         
@@ -142,10 +144,56 @@ class call:
         elif request['status'] == 'SUCCESSFUL':
             # if clickstream data, return download links, else return download link for 
             if request['exportType'] == 'RESEARCH_EVENTING':
-                # Get clickstream download links
-                return 'clickstreams_ready'
+                # Create request for links
+                CLL = ClickstreamDownloadLinksRequest(course_id = self.course_id, 
+                                                      interval = self.interval)
+                # Get links --> add try / except
+                links = api.get_clickstream_download_links(CLL)
+                return links
             else:
+                # This is table (sql) data.
                 return request['download_link']
             
     '''
-    Download 
+    Download data 
+    '''
+    
+    '''
+    MERGE DOWNLOAD_URL INTO DOWNLOAD FUNCTION BELOW!!!
+    '''
+
+    def download_url(url, dest_folder):
+        """
+        Download url to dest_folder/FILENAME, where FILENAME is the last
+        part of the url path.
+        """
+        filename = urlparse(url).path.split('/')[-1]
+        full_filename = os.path.join(dest_folder, filename)
+        response = requests.get(url, stream=True)
+        chunk_size = 1024 * 1024
+        logging.debug('Writing to file: {}'.format(full_filename))
+
+        with open(full_filename, 'wb') as f:
+            for data in tqdm(
+                        iterable=response.iter_content(chunk_size),
+                        total=int(response.headers['Content-length']) / chunk_size,
+                        unit='MB',
+                        desc=filename):
+                f.write(data)
+        return full_filename
+    
+    def download(self, link):
+        
+        data_folder = os.chdir() + "/data/"
+        
+        # Check if 'data' folder exists
+        if not os.path.exists(data_folder):
+            os.makedirs(data_folder)
+        
+        # Check if course slug folder exists in data folder
+        if not os.path.exists(self.folder):
+            os.makedirs(self.folder)
+            
+        # Download file to folder
+        download_url(link, self.folder)
+        
