3d8ef7330dc6c9e8be2a4635948fb6b2b4621780
Jasper Ginn <jasperginn@hotmail.com>
2016-10-08 18:29:40 +0200

    Argparser now works as expected

--- call.py
+++ call.py
@@ -25,27 +25,44 @@ limitations under the License.
 import argparse
 from scheduler import coursera
 from store import gcloud
+import os
 
 if __name__=="__main__":
 
     # Set up parser and add arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("type", help="Either one of 'clickstream' or 'tables'", type=str, choices=["clickstream", "tables"])
-    parser.add_argument("course_slugs", help="Location of a file containing course slug names. Each slug should be placed on a new line.", type=str)
-    parser.add_argument("-c", "--copy_to_gcloud", help="Copy the downloaded files to a mounted gcloud bucket. Use the '--mount_location' argument to specify the mounted gcloud directory and '--bucket_name' to specify the bucket name", action="store_true", type=str)
+    parser.add_argument("export_type", help="Either one of 'clickstream' or 'tables'", type=str, choices=["clickstream", "tables"])
+    parser.add_argument("course_slugs", help="EITHER: A course slug name, OR: Location of a text file (.txt) containing multiple course slug names. Each slug should be placed on a new line.", type=str)
+    parser.add_argument("-c", "--copy_to_gcloud", help="Copy the downloaded files to a mounted gcloud bucket. Use the '--mount_location' argument to specify the mounted gcloud directory and '--bucket_name' to specify the bucket name", action="store_true")
     parser.add_argument("-m", "--gcloud_mount_location", help="The directory where the gcloud directory is mounted.", type=str)
     parser.add_argument("-b", "--bucket_name", help="Name of the gcloud bucket.", type=str)
     args = parser.parse_args()
 
     # Check arguments that are supposed to go together
-    req = c["copy_to_gcloud", "gcloud_mount_location", "bucket_name"]
-    if not all([getattr(args, x) for x in req]):
-        raise RuntimeError("Please supply the gcloud bucket mount location on your local system and the bucket name for the bucket.")
+    if args.copy_to_gcloud:
+        if args.gcloud_mount_location == None or args.bucket_name == None:
+            raise RuntimeError("Please supply the gcloud mount directory and the bucket name.")
 
     # Check directories
-    if not os.path.exists(args.gcloud_mount_location):
-        raise RuntimeError("gcloud bucket is not mounted at path {}".format(args.gcloud_mount_location))
+    if args.gcloud_mount_location != None:
+        if not os.path.exists(args.gcloud_mount_location):
+            raise RuntimeError("gcloud bucket is not mounted at path {}. Directory does not exist.".format(args.gcloud_mount_location))
 
+    # Check slugs. If slug is mispelled it will be caught downstream in the coursera class.
+    if ".txt" in args.course_slugs:
+        with open(args.course_slugs, 'r') as inFile:
+            courseSlugs = [line.replace("\n", "") for line in inFile]
+    else:
+        # Check if a filepath
+        if os.path.isfile(args.course_slugs):
+            raise RuntimeError("You entered a file path but the destination file is not a .txt file.")
+        # Else check if a mispelled filepath
+        elif "/" in args.course_slugs:
+            raise RuntimeError("You entered an invalid file path containing a destination file that is not a .txt file.")
+        else:
+            courseSlugs = args.course_slugs
+
+    print courseSlugs
     '''
     Call download class
     '''
