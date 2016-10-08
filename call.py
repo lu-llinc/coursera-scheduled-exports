858b6b853f47531cca95b31984eae2719d708ae6
Jasper Ginn <jasperginn@hotmail.com>
2016-10-08 17:44:56 +0200

    Added apache license notice

--- call.py
+++ call.py
@@ -1,6 +1,22 @@
 #! /usr/bin/env python
 
 '''
+Copyright 2016 Leiden University
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+'''
 # Call scheduler.py and store.py to download files and store them in a google cloud bucket
 # Jasper Ginn
 # 07/10/2016
@@ -11,7 +27,7 @@ from scheduler import coursera
 from store import gcloud
 
 if __name__=="__main__":
-    
+
     # Set up parser and add arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("type", help="Either one of 'clickstream' or 'tables'", type=str, choices=["clickstream", "tables"])
@@ -20,20 +36,20 @@ if __name__=="__main__":
     parser.add_argument("-m", "--gcloud_mount_location", help="The directory where the gcloud directory is mounted.", type=str)
     parser.add_argument("-b", "--bucket_name", help="Name of the gcloud bucket.", type=str)
     args = parser.parse_args()
-    
+
     # Check arguments that are supposed to go together
     req = c["copy_to_gcloud", "gcloud_mount_location", "bucket_name"]
     if not all([getattr(args, x) for x in req]):
         raise RuntimeError("Please supply the gcloud bucket mount location on your local system and the bucket name for the bucket.")
-    
+
     # Check directories
     if not os.path.exists(args.gcloud_mount_location):
         raise RuntimeError("gcloud bucket is not mounted at path {}".format(args.gcloud_mount_location))
-    
+
     '''
     Call download class
     '''
-    
+
     '''
     Call store class
     '''
--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,23 @@
 #! /usr/bin/env python
 
 '''
-# Schedule coursera research exports downloads 
+Copyright 2016 Leiden University
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+'''
+# Schedule coursera research exports downloads
 # Jasper Ginn
 # 07/10/2016
 '''
@@ -22,22 +38,22 @@ Create a request to the API
 '''
 
 class coursera:
-    
+
     def __init__(self, course_slug):
         self.course_slug = course_slug
         self.interval = [str(datetime.date.today() - datetime.timedelta(days=7)), # If you're running it on friday, you want the results from
                          str(datetime.date.today() - datetime.timedelta(days=1))] # Previous friday to yesterday.]
         self.folder = os.getcwd() + "/data/" + course_slug + "/"
-        
+
         # Set up logger
-        logging.basicConfig(filename = "scheduler.log", filemode='w', format='%(asctime)s %(message)s', 
+        logging.basicConfig(filename = "scheduler.log", filemode='w', format='%(asctime)s %(message)s',
                             level=logging.INFO)
         logging.info("Started download for course {}".format(course_slug))
-        
+
     '''
     Retrieve course id based on course slug
     '''
-    
+
     def get_course_id(self):
         # Link below retrieves JSON file with course information based on course name
         base_url = "https://www.coursera.org/api/onDemandCourses.v1?q=slug&slug="
@@ -55,17 +71,17 @@ class coursera:
         course_id = json_data["elements"][0]["id"]
         # Return
         self.course_id = course_id
-        
+
     '''
     Request sql data
     '''
-    
-    def request_schemas(self, export_type = "RESEARCH_WITH_SCHEMAS", anonymity_level = "HASHED_IDS_NO_PII", 
+
+    def request_schemas(self, export_type = "RESEARCH_WITH_SCHEMAS", anonymity_level = "HASHED_IDS_NO_PII",
                         statement_of_purpose = "Weekly backup of course data",
-                        schema_names = ["demographics",	
-                                        "users",	
+                        schema_names = ["demographics",
+                                        "users",
                                         "course_membership",
-                                        "course_progress",	
+                                        "course_progress",
                                         "feedback",
                                         "assessments",
                                         "course_grades",
@@ -73,11 +89,11 @@ class coursera:
                                         "discussions",
                                         "programming_assignments",
                                         "course_content"]):
-                                            
+
         logging.info("Requesting table data ({})".format(self.course_slug))
-        
+
         # Construct request
-        er = ExportRequest(course_id=self.course_id, export_type=export_type, anonymity_level = anonymity_level, 
+        er = ExportRequest(course_id=self.course_id, export_type=export_type, anonymity_level = anonymity_level,
                            statement_of_purpose = statement_of_purpose, schema_names = schema_names)
         # Fire request
         try:
@@ -85,27 +101,27 @@ class coursera:
         except: # Find out specific error
             logging.error("Request failed ({})".format(self.course_slug))
             raise ValueError("Failed request")
-        
+
         logging.info("Request successfull ({})".format(self.course_slug))
-        
+
         # Add info to self
         vals = ERM.to_json()
         self.id_ = vals['id']
         self.type_ = "TABLE"
         self.metadata = vals["metadata"]
         self.schemaNames = vals["schemaNames"]
-        
+
     '''
     Request clickstream data
     '''
-    
-    def request_clickstream(self, export_type = "RESEARCH_EVENTING", anonymity_level = "HASHED_IDS_NO_PII", 
+
+    def request_clickstream(self, export_type = "RESEARCH_EVENTING", anonymity_level = "HASHED_IDS_NO_PII",
                             statement_of_purpose = "Weekly backup of course data"):
-                                            
+
         logging.info("Requesting clickstream data ({}) for period {} to {}".format(self.course_slug, self.interval[0], self.interval[1]))
-        
+
         # Construct request
-        er = ExportRequest(course_id=self.course_id, export_type=export_type, anonymity_level = anonymity_level, 
+        er = ExportRequest(course_id=self.course_id, export_type=export_type, anonymity_level = anonymity_level,
                            statement_of_purpose = statement_of_purpose, interval=self.interval)
         # Fire request
         try:
@@ -113,32 +129,32 @@ class coursera:
         except:
             logging.error("Request failed ({})".format(self.course_slug))
             raise ValueError("Failed request")
-        
+
         logging.info("Request successfull ({})".format(self.course_slug))
-        
+
         # Add id to self
         vals = ERM.to_json()
         self.id_ = vals['id']
         self.type_ = "CLICKSTREAM"
         self.metadata = vals["metadata"]
-        
+
     '''
     Check if download is ready every 10 minutes
     '''
-    
+
     def status_export(self, interval = 600):
-        
+
         # Get status of export download
         request = api.get(self.id_)[0].to_json()
-        
+
         # If ready, return download link; if not, sleep for interval time
         if request['status'] == 'IN_PROGRESS':
             time.sleep(interval) # Add a maximum wating time (e.g. ~4 hours). Else, log error and continue with next course
         elif request['status'] == 'SUCCESSFUL':
-            # if clickstream data, return download links, else return download link for 
+            # if clickstream data, return download links, else return download link for
             if request['exportType'] == 'RESEARCH_EVENTING':
                 # Create request for links
-                CLL = ClickstreamDownloadLinksRequest(course_id = self.course_id, 
+                CLL = ClickstreamDownloadLinksRequest(course_id = self.course_id,
                                                       interval = self.interval)
                 # Get links --> add try / except
                 links = api.get_clickstream_download_links(CLL)
@@ -149,13 +165,13 @@ class coursera:
         else:
             logging.error("Unknown status <{}> returned by api".format(request['status']))
             raise ValueError("Unknown status returned by api")
-            
+
     '''
-    Download data 
+    Download data
     '''
-    
+
     def download(self, link):
-        
+
         data_folder = os.getcwd() + "/data/"
         # Check if 'data' folder exists
         if not os.path.exists(data_folder):
@@ -163,20 +179,20 @@ class coursera:
         # Check if course slug folder exists in data folder
         if not os.path.exists(self.folder):
             os.makedirs(self.folder)
-        
+
         logging.info("Downloading file ({})".format(course_slug))
-        
+
         resp = utils.download_url(link, self.folder)
-        
+
         # Return
         return self.folder
-        
+
     '''
     Add metadata
     '''
-    
+
     def return_metadata(self):
-        
+
         # Create metadata
         mt = {"course":self.course_slug, "course_id":self.course_id, "exportType":self.type_,
               "meta":self.metadata, "path":self.folder}
