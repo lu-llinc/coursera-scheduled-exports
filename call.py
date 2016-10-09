59c9d4d52e02c0de2676bb90203e3fd89f980c85
Jasper Ginn <jasperginn@hotmail.com>
2016-10-09 21:47:41 +0200

    turning into string

--- call.py
+++ call.py
@@ -68,7 +68,7 @@ def coursera_download(course_slug, request_type, location, store_metadata = True
     # Get metadata and store in file
     if store_metadata:
         meta = c.return_metadata()
-        time_now = datetime.datetime.now()
+        time_now = str(datetime.datetime.now())
         with open("{}/metadata.txt".format(location), 'a') as inFile:
             inFile.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(time_now.encode("utf8"), meta["course"].encode("utf8"),
                                                          meta["course_id"].encode("utf8"), meta["exportType"].encode("utf8"),
