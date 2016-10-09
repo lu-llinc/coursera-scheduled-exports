ece2d1e3d45d353f058f140bd0841792fab29a82
Jasper Ginn <jasperginn@hotmail.com>
2016-10-09 21:00:48 +0200

    Added gsfuse

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,12 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install bzip2 libpq-dev
+# Install gsfuse
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install gcsfuse
 # Install anaconda
 wget https://repo.continuum.io/archive/Anaconda2-4.2.0-Linux-x86_64.sh
 sudo bash Anaconda2-4.2.0-Linux-x86_64.sh -b -p /opt/anaconda
