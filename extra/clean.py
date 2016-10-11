d52ca495e93493ba3e98a202762e672da1044412
Jasper Ginn <jasperginn@hotmail.com>
2016-10-11 12:38:44 +0200

    First version of clean script

--- extra/clean.py
+++ extra/clean.py
@@ -27,6 +27,16 @@ import os
 import datetime
 
 '''
+Exceptions
+'''
+
+class nopath(Exception):
+    pass
+
+class emptyfolders(Exception):
+    pass
+
+'''
 clean
 '''
 
@@ -36,4 +46,31 @@ class cleaner:
         self.location = location
         self.files_to_keep = files_to_keep
 
-    def scope()
+        # Check if real location
+        if not os.path.exists(location):
+            raise nopath("The location you entered does not exist. Please enter a valid location.")
+
+        # Check if 'tables' folder exists
+        if not os.path.exists("{}/tables/".format(location)):
+            raise nopath("'Tables' folder does not exist in this directory.")
+
+        # If all folders empty
+        folders = [d[0] for d in os.walk("{}/tables/".format(location))]
+        del folders[0]
+        folders_with_files = [f for f in folders if len(os.listdir(f)) >= files_to_keep]
+        if len(folders_with_files) < 1:
+            raise emptyfolders("All folders have less than {} files.".format(str(files_to_keep)))
+        # Add to self
+        self.folders = folders_with_files
+
+    def scope():
+        # For each folder, delete oldest
+        for fold in self.folders:
+            # List files and ignore system files
+            f = [file_ for file_ in os.listdir(fold) if not file_.startswith('.')]
+            # If length f > files_to_keep
+            if len(f) >= files_to_keep:
+                # Take out oldest files
+                diff = (len(f) - files_to_keep) - 1
+                for d in range(0,diff):
+                    os.remove(f[d])
