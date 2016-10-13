6ecddb5b57fcb4604882c9aaa598a4c3e61753db
Jasper Ginn <jasperginn@hotmail.com>
2016-10-13 11:44:01 +0200

    Removed double download command

--- call.py
+++ call.py
@@ -99,7 +99,6 @@ def coursera_download(course_slug, request_type, location, store_metadata = True
             if args.log:
                 logging.error("File {} is incomplete. Skipping download of this file. Check if there are days missing in your download folder {}.".format(filename, tloc))
 		continue
-        c.download(link, tloc)
 	try:
             c.download(link, tloc)
 	except:
